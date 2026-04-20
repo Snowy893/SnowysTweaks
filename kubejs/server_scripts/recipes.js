63a0053fdba7666450cf7f47bb03ff6584362313
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-04-20 01:49:19 -0400

    Farmer's Delight fried egg can now be made with any egg

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,5 +1,10 @@
-console.info('Loaded Snowy\'s Recipe Tweaks')
 ServerEvents.recipes(event => {
 	event.remove({ id: 'minecraft:lead' })
 	event.remove({ id: 'minecraft:lodestone' })
+	event.replaceInput(
+		{ output: 'farmersdelight:fried_egg' },
+		'minecraft:egg',
+		'#c:eggs'
+	)
 })
+console.info('Loaded Snowy\'s Recipe Tweaks')
