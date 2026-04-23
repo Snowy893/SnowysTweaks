953955868ae691e21b0165163b8db050fc886312
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-04-23 18:49:47 -0400

    Removed sulfar item entry from KubeJS, instead made sulfar ore generate way less and drop only 1 sulfar per ore (more with fortune ofc)

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,5 +1,3 @@
-ItemEvents.modification(event => {
-    event.modify('sulfar_mod:sulfar', item => item.burnTime = 800)
-    event.modify('sulfar_mod:sulfar_block', item => item.burnTime = 8000)
-})
+ItemEvents.modification(event =>
+    event.modify('sulfar_mod:sulfar_block', item => item.burnTime = 18000))
 console.info('Loaded Snowy\'s Item Tweaks')
