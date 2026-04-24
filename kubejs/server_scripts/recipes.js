ca8665297048c72a76f01eb38b6915897fdae74c
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-04-24 06:10:24 -0400

    Added back recipes script that I think I accidentally deleted, replaced `hexalia:salt` with `hearthandharvest:salt`, added way to get salt from `biomesoplenty:dried_salt`

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,44 @@
+ServerEvents.recipes(event => {
+    event.remove({ id: 'minecraft:lead' })
+    event.remove({ id: 'minecraft:lodestone' })
+    event.remove({ id: 'minecraft:powered_rail' })
+    event.remove({ id: 'sulfar_mod:blaze_powder_sulfar_recipe' })
+    event.remove({ id: 'hexalia:salt' })
+    event.remove({ id: 'hexalia:salt_from_mortar' })
+
+    event.replaceInput({ output: 'farmersdelight:fried_egg' }, 'minecraft:egg', '#c:eggs')
+    event.replaceInput({ input: 'hexalia:salt' }, 'hexalia:salt', 'hearthandharvest:salt')
+    event.replaceOutput({ output: 'hexalia:salt' }, 'hexalia:salt', 'hearthandharvest:salt')
+
+    event.custom({
+        type: 'hexalia:mortar_and_pestle',
+        ingredients: [
+            {
+                item: 'hexalia:saltsprout'
+            }
+        ],
+        output: {
+            item: 'hearthandharvest:salt'
+        }
+    })
+    event.custom({
+        type: 'farmersdelight:cooking',
+        cookingtime: 100,
+        experience: 0.35,
+        ingredients: [
+            {
+                item: 'biomesoplenty:dried_salt'
+            }
+        ],
+        recipe_book_tab: 'misc',
+        result: {
+            count: 8,
+            item: 'hearthandharvest:salt'
+        }
+    })
+})
+
+LootJS.modifiers(event =>
+    event.addBlockLootModifier("hexalia:salt_block").replaceLoot("hexalia:salt", "hearthandharvest:salt", true))
+
+console.info('Loaded Snowy\'s Recipe Tweaks')
