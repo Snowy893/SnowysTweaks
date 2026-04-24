b8d554c6cc27aff05d56282565ed3be7a759f197
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-04-24 04:42:17 -0400

    Increased sulfar block burn time to be the same ratio of that between coal and coal blocks

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,3 +1,3 @@
 ItemEvents.modification(event =>
-    event.modify('sulfar_mod:sulfar_block', item => item.burnTime = 18000))
+    event.modify('sulfar_mod:sulfar_block', item => item.burnTime = 20000))
 console.info('Loaded Snowy\'s Item Tweaks')
